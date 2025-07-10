7996e3f8e384a34e12df9518565c16e9167ba4d2
caddySpoon <cwb5116@gmail.com>
2025-07-11 00:08:44 +0900

    8. 플러그인으로 빌드 확장하기

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // 템플릿 HTML
+      filename: 'index.html', // 출력될 HTML 파일 이름
+      inject: true, // <script> 태그 자동 삽입
+    }),
+  ],
   module: {
     rules: [
       {
